7b9196d3dd005de55a5e4c500cc79b2d3991bb28
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-02-07 00:35:39 +0530

    fix:cors origin of vercel

--- server/server.js
+++ server/server.js
@@ -10,11 +10,15 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: true,
-  credentials: true,
-}));
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // local Vite dev
+      "https://skill-swap-fullstack.vercel.app" // production frontend
+    ],
+    credentials: true
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
