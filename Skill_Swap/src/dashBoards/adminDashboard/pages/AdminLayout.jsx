592212cf056f3458be8cca6bc483709a569a2c6c
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-02-07 01:48:09 +0530

    fixed the adminisde navbar

--- Skill_Swap/src/dashBoards/adminDashboard/pages/AdminLayout.jsx
+++ Skill_Swap/src/dashBoards/adminDashboard/pages/AdminLayout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import AdminSidebar from "../AdminSidebar";
 
 const AdminLayout = () => {
   return (
-    <div className="admin-layout">
-      {/* sidebar / navbar */}
-      <Outlet />
+    <div className="flex min-h-screen">
+      {/* ADMIN SIDEBAR */}
+      <AdminSidebar/>
+
+      {/* PAGE CONTENT */}
+      <main className="ml-64 flex-1 bg-gray-100 p-6">
+        <Outlet />
+      </main>
     </div>
   );
 };
