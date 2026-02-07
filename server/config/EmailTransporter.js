b55852c12d8a25498593f340cc39b8cba2a44a5a
Balagonisanthosh <balagonisanthosh445@gmail.com>
2026-02-07 22:34:25 +0530

    fixed nodemail function

--- server/config/EmailTransporter.js
+++ server/config/EmailTransporter.js
@@ -2,11 +2,14 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,          
+  secure: false,      
   auth: {
-    user: process.env.EMAIL_USER, 
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  }
+  },
+  family: 4           
 });
 
 module.exports = transporter;
